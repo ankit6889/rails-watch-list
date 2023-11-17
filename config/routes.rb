249ac737af0c3b54b2f1cacc8f0f7aaccf512a99
Ankit Kumar <ankit@gypsyphi.com>
2023-11-17 12:51:39 +0800

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists do
+  root to: 'lists#index'
+  resources :lists, except: :index do
     resources :bookmarks, except: :destroy
   end
   resources :bookmarks, only: :destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@ url = "https://tmdb.lewagon.com/movie/top_rated"
 user_serialized = URI.open(url).read
 user = JSON.parse(user_serialized)
 
-user['results'].first(50).each do |movie|
+user['results'].each do |movie|
   Movie.create(title: movie['title'], overview: movie['overview'], poster_url: "https://image.tmdb.org/t/p/original#{movie['poster_path']}", rating: movie['vote_average'])
 end
